ac00834f575b9a8e1957ed797310cae04df21978
Roger <12768403+rogix@users.noreply.github.com>
2019-09-04 23:46:06 -0300

    chores: add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 
+import GlobalStyle from "./styles/global";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <GlobalStyle />;
 }
 
 export default App;
